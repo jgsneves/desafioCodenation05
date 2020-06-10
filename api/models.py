9c72bc15a2b487ec08deba2eb6bd1ad89a37681c
João Neves <joao.neves@enecad.com.br>
2020-06-10 18:07:32 -0300

    nota maxima

--- api/models.py
+++ api/models.py
@@ -1,21 +1,55 @@
 from django.db import models
+from django.core.validators import (
+    EmailValidator,
+    MinLengthValidator,
+    validate_ipv4_address,
+    ValidationError
+)
+# Create your models here.
+
+
+def event_level_validate(event):
+    if event not in (
+        'CRITICAL',
+        'DEBUG',
+        'ERROR',
+        'WARNING',
+            'INFO'):
+        raise ValidationError('Level not found')
 
 
 class User(models.Model):
-    raise NotImplementedError
+    name = models.CharField('Nome', max_length=50)
+    last_login = models.DateTimeField('Ultimo Acesso', auto_now=True)
+    email = models.CharField('Email', max_length=254, validators=[
+                             EmailValidator('Insira um email válido')])
+    password = models.CharField('Senha', max_length=50, validators=[
+                                MinLengthValidator(8)])
 
 
 class Agent(models.Model):
-    raise NotImplementedError
+    name = models.CharField('Nome', max_length=50)
+    status = models.BooleanField('Status')
+    env = models.CharField('Env', max_length=20)
+    version = models.CharField('Versao', max_length=5)
+    address = models.CharField('Endereco', max_length=39, validators=[
+                               validate_ipv4_address])
 
 
 class Event(models.Model):
-    raise NotImplementedError
+    level = models.CharField('Level', max_length=20,
+                             validators=[event_level_validate])
+    data = models.TextField('Dado')
+    arquivado = models.BooleanField('Arquivado')
+    date = models.DateField('Data', auto_now=True)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Group(models.Model):
-    raise NotImplementedError
+    name = models.CharField(max_length=50)
 
 
 class GroupUser(models.Model):
-    raise NotImplementedError
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- datadesign/api/models.py
+++ datadesign/api/models.py
@@ -1,30 +1,49 @@
 from django.db import models
-
+from django.core.validators import (
+    EmailValidator,
+    MinLengthValidator,
+    validate_ipv4_address,
+    ValidationError
+)
 # Create your models here.
 
 
+def event_level_validate(event):
+    if event not in (
+        'CRITICAL',
+        'DEBUG',
+        'ERROR',
+        'WARNING',
+            'INFO'):
+        raise ValidationError('Level not found')
+
+
 class User(models.Model):
-    name = models.CharField(max_length=50)
-    last_login = models.DateTimeField()
-    email = models.CharField(max_length=254)
-    password = models.CharField(max_length=50)
+    name = models.CharField('Nome', max_length=50)
+    last_login = models.DateTimeField('Ultimo Acesso', auto_now=True)
+    email = models.CharField('Email', max_length=254, validators=[
+                             EmailValidator('Insira um email válido')])
+    password = models.CharField('Senha', max_length=50, validators=[
+                                MinLengthValidator(8)])
 
 
 class Agent(models.Model):
-    name = models.CharField(max_length=50)
-    status = models.BooleanField()
-    env = models.CharField(max_length=20)
-    version = models.CharField(max_length=5)
-    address = models.CharField(max_length=39)
+    name = models.CharField('Nome', max_length=50)
+    status = models.BooleanField('Status')
+    env = models.CharField('Env', max_length=20)
+    version = models.CharField('Versao', max_length=5)
+    address = models.CharField('Endereco', max_length=39, validators=[
+                               validate_ipv4_address])
 
 
 class Event(models.Model):
-    level = models.CharField(max_length=20)
-    data = models.TextField()
-    arquivado = models.BooleanField()
-    date = models.DateField()
-    agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    level = models.CharField('Level', max_length=20,
+                             validators=[event_level_validate])
+    data = models.TextField('Dado')
+    arquivado = models.BooleanField('Arquivado')
+    date = models.DateField('Data', auto_now=True)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Group(models.Model):
@@ -32,5 +51,5 @@ class Group(models.Model):
 
 
 class GroupUser(models.Model):
-    group = models.ForeignKey('Group', on_delete=models.CASCADE)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
